6a574071ff7c9fa19e66ef178284feaf69eddab7
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-01 10:52:50 -0300

    Commit inicial de um programa que converte Dólar para Real (atividade do Senai).

---
+++ ex_fixacao01.py
@@ -0,0 +1,11 @@
+'''
+Fazer um programa que pergunte um valor em Dólares e converta para Reais. considere que 1 Dólar vale 4,78 reais.
+'''
+
+print("Insira um valor em Dólares $:")
+
+doll = float(input("Inserir valor:"))
+
+doll = doll * 4.74
+
+print("O valor em reais é:", doll,"R$")
